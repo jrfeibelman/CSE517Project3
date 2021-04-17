9e908dbb692791afae3433536c2fd6bb3409ecd3
jrfibel <jason.feibelman@gmail.com>
2021-04-17 11:04:57 -0500

    initial bias attempt

--- recoverBias.py
+++ recoverBias.py
@@ -15,9 +15,22 @@ Solves for the hyperplane bias term, which is uniquely specified by the support 
 import numpy as np
 
 def recoverBias(K,yTr,alphas,C):
-    bias = 0
+      
+    i_star = find_i_star(alphas,C)
     
-    
-    
+    bias = 1/yTr[i_star] - K[i_star,:] @ (alphas * yTr)
+        
     return bias 
     
+def find_i_star(alphas,C):
+    """
+    Return index of element in alpha that is furthest from 0 and C
+    """
+    max_dist = abs(alphas[0] - C) + alphas[0]
+    max_idx = 0
+    for i in range(1,len(alphas)):
+        if max_dist < abs(alphas[i] - C) + alphas[i]:
+            max_dist = abs(alphas[i] - C) + alphas[i]
+            max_idx = i
+                   
+    return max_idx
