89df1019710fa7a4bb1087886a540b7baeaa98a2
jrfibel <jason.feibelman@gmail.com>
2021-04-17 20:56:00 -0500

    added bound 0<a<C

--- recoverBias.py
+++ recoverBias.py
@@ -15,21 +15,28 @@ Solves for the hyperplane bias term, which is uniquely specified by the support 
 import numpy as np
 
 def recoverBias(K,yTr,alphas,C):
-
+    
     i_star = find_i_star(alphas,C)
     
-    bias = 1/yTr[i_star] - K[i_star,:] @ (alphas * yTr)
+    bias = 1.0/yTr[i_star] - K[i_star,:] @ (alphas * yTr)
         
     return bias 
     
 def find_i_star(alphas,C):
     """
     Return index of element in alpha that is furthest from 0 and C
+    0 < a < C
     """
-    max_dist = abs(alphas[0] - C) + alphas[0]
+    max_dist = -1
     max_idx = 0
-    for i in range(1,len(alphas)):
+        
+    for i in range(len(alphas)):
+        
+        if alphas[i] >= C or alphas[i] <= 0:
+            continue
+        
         curr = abs(alphas[i] - C) + alphas[i]
+        
         if max_dist < curr:
             max_dist = curr
             max_idx = i
